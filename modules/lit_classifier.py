cbb85370c89f545e8a9f7ed9a34182b2a7cd4c0b
Mootez <mootez.saad@medtech.tn>
2023-03-19 15:32:06 -0300

    update logging and metric calc

--- modules/lit_classifier.py
+++ modules/lit_classifier.py
@@ -15,6 +15,14 @@ class PlastDectClassifier(pl.LightningModule):
         self.acc = torchmetrics.Accuracy(task='binary')
         self.f1 = torchmetrics.F1Score(task='binary')
         self.mcc = torchmetrics.MatthewsCorrCoef(task='binary')
+
+        self.acc_val = torchmetrics.Accuracy(task='binary')
+        self.f1_val = torchmetrics.F1Score(task='binary')
+        self.mcc_val = torchmetrics.MatthewsCorrCoef(task='binary')
+
+        self.acc_test = torchmetrics.Accuracy(task='binary')
+        self.f1_test = torchmetrics.F1Score(task='binary')
+        self.mcc_test = torchmetrics.MatthewsCorrCoef(task='binary')
         #### Optimizer params
         self.lr=lr
         self.weight_decay=weight_decay
@@ -32,13 +40,9 @@ class PlastDectClassifier(pl.LightningModule):
         loss = self.loss_func(logits, y)
         preds = torch.argmax(logits, dim=1)
         preds = preds.float().unsqueeze(1)
-        acc = self.acc(preds, y)
-        f1 = self.f1(preds, y)
-        mcc = self.mcc(preds, y)
-        self.log('train_loss', loss,  on_epoch=True, logger=True)
-        self.log('train_acc', acc,  on_epoch=True, logger=True)
-        self.log('train_f1', f1,  on_epoch=True, logger=True)
-        self.log('train_mcc', mcc,  on_epoch=True, logger=True)
+        self.acc.update(preds, y)
+        self.f1.update(preds, y)
+        self.mcc.update(preds, y)
 
         return loss
     
@@ -49,13 +53,9 @@ class PlastDectClassifier(pl.LightningModule):
         loss = self.loss_func(logits, y)
         preds = torch.argmax(logits, dim=1)
         preds = preds.float().unsqueeze(1)
-        acc = self.acc(preds, y)
-        f1 = self.f1(preds, y)
-        mcc = self.mcc(preds, y)
-        self.log('val_loss', loss,  on_epoch=True, logger=True)
-        self.log('val_acc', acc,  on_epoch=True, logger=True)
-        self.log('val_f1', f1,  on_epoch=True, logger=True)
-        self.log('val_mcc', mcc,  on_epoch=True, logger=True)
+        self.acc_val.update(preds, y)
+        self.f1_val.update(preds, y)
+        self.mcc_val.update(preds, y)
 
         return loss
     
@@ -66,16 +66,33 @@ class PlastDectClassifier(pl.LightningModule):
         loss = self.loss_func(logits, y)
         preds = torch.argmax(logits, dim=1)
         preds = preds.float().unsqueeze(1)
-        acc = self.acc(preds, y)
-        f1 = self.f1(preds, y)
-        mcc = self.mcc(preds, y)
+        acc = self.acc_test(preds, y)
+        f1 = self.f1_test(preds, y)
+        mcc = self.mcc_test(preds, y)
         self.log('test_loss', loss,  on_epoch=True, logger=True)
         self.log('test_acc', acc,  on_epoch=True, logger=True)
         self.log('test_f1', f1,  on_epoch=True, logger=True)
         self.log('test_mcc', mcc,  on_epoch=True, logger=True)
 
         return loss
+    
+    def training_epoch_end(self, outs):
+        self.log('train_acc', self.acc)
+        self.log('train_f1', self.f1)
+        self.log('train_mcc', self.mcc)
+        # Reset
+        self.acc.reset()
+        self.f1.reset()
+        self.mcc.reset()
         
+    def validation_epoch_end(self, outs):
+        self.log('val_acc', self.acc_val)
+        self.log('val_f1', self.f1_val)
+        self.log('val_mcc', self.mcc_val)
+        # Reset
+        self.acc_val.reset()
+        self.f1_val.reset()
+        self.mcc_val.reset()
 
     def configure_optimizers(self):
         optimizer = Adam(self.parameters(), lr=1e-3, weight_decay=1e-2)
